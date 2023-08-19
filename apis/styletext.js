bad034f38958b9a38352ac5c2ef2b2863d53c4bc
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2023-08-19 20:46:33 -0300

    Update styletext.js

--- apis/styletext.js
+++ apis/styletext.js
@@ -8,20 +8,6 @@ const FormData = require('form-data')
 const request = require("request")
 const { rest } = require("lodash")
 
-function parseFileSize(size) {
-    return parseFloat(size) * (/GB/i.test(size)
-        ? 1000000
-        : /MB/i.test(size)
-            ? 1000
-            : /KB/i.test(size)
-                ? 1
-                : /bytes?/i.test(size)
-                    ? 0.001
-                    : /B/i.test(size)
-                        ? 0.1
-                        : 0);
-}
-
 function styletext(teks) {
     return new Promise((resolve, reject) => {
         axios.get('http://qaz.wtf/u/convert.cgi?text='+teks)
