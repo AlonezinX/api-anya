20e0e53f85bb88d25ef1dc0a005d04c53da4791c
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2023-08-19 18:39:57 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -28,7 +28,7 @@ var router  = express.Router();
 
 var { spawn, exec } = require('child_process');
 var { color, bgcolor } = require(__path + '/lib/color.js');
-var { styletext } = require(__path + '/apis/styletext.js');
+var { styletext } = require(__path + '/lib/listdl');
 
 precisos = {
     digitarapikey: {
