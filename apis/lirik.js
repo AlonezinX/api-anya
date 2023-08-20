aebb7a89e9dcc2640860457741650e28697f8dff
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2023-08-20 08:38:50 -0300

    Update lirik.js

--- apis/lirik.js
+++ apis/lirik.js
@@ -20,4 +20,18 @@ return hasil
   })
 }
 
-module.exports = { lirikLagu }
+function covid() {
+  return new Promise(async(resolve, reject) => {
+const res = await axios.get(`https://www.worldometers.info/coronavirus/country/indonesia/`) 
+const $ = cheerio.load(res.data)
+hasil = []
+a = $('div#maincounter-wrap')
+kasus = $(a).find('div > span').eq(0).text()
+kematian = $(a).find('div > span').eq(1).text() 
+sembuh = $(a).find('div > span').eq(2).text() 
+hasil.push({ kasus, kematian, sembuh}) 
+return hasil
+  })
+}
+
+module.exports = { lirikLagu, covid }
