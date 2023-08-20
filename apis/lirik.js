674b0987aad3bca7f6998caf35a082fc19a4813c
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2023-08-20 09:34:30 -0300

    Update lirik.js

--- apis/lirik.js
+++ apis/lirik.js
@@ -30,5 +30,18 @@ hasil.push({ kasus, kematian, sembuh})
 return hasil
 }
 
+const wikiSearch = async (query) => {
+const res = await axios.get(`https://pt.m.wikipedia.org/wiki/${query}`)
+const $ = cheerio.load(res.data)
+const hasil = []
+let wiki = $('#mf-section-0').find('p').text()
+let thumb = $('#mf-section-0').find('div > div > a > img').attr('src')
+thumb = thumb ? thumb : '//pngimg.com/uploads/wikipedia/wikipedia_PNG35.png'
+thumb = 'https:' + thumb
+let titulo = $('h1#section_0').text()
+hasil.push({ wiki, thumb, titulo })
+return hasil
+  }
 
-module.exports = { lirikLagu, covid }
+
+module.exports = { lirikLagu, covid, wikiSearch }
