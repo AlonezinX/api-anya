ce465f07a505fb529639e618141b7980e0c5dd39
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2023-08-20 09:48:08 -0300

    Update lirik.js

--- apis/lirik.js
+++ apis/lirik.js
@@ -1,5 +1,6 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
+const ytdl = require('ytdl-core');
 
 const lirikLagu = async (query) => {
 const res = await axios.get(`https://www.musixmatch.com/search/${query}`)
@@ -43,5 +44,86 @@ hasil.push({ wiki, thumb, titulo })
 return hasil
   }
 
+function ytMp4(url) {
+    return new Promise((async(resolve, reject) => {
+        ytdl.getInfo(url).then(async(getUrl) => {
+            let result = [];
+            for(let i = 0; i < getUrl.formats.length; i++) {
+                let item = getUrl.formats[i];
+                if (item.container == 'mp4' && item.hasVideo == true && item.hasAudio == true) {
+                    let { qualityLabel, contentLength } = item;
+                    let bytes = await bytesToSize(contentLength);
+                    result[i] = {
+                        video: item.url,
+                        quality: qualityLabel,
+                        size: bytes
+                    };
+                };
+            }
+            let resultFix = result.filter(x => x.video != undefined && x.size != undefined && x.quality != undefined) 
+            let tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].video}`);
+            let tinyUrl = tiny.data;
+            let title = getUrl.videoDetails.title;
+            let desc = getUrl.videoDetails.description;
+            let views = millify(getUrl.videoDetails.viewCount)
+            let channel = getUrl.videoDetails.ownerChannelName;
+            let uploadDate = getUrl.videoDetails.uploadDate;
+            let thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
+            resolve({
+                title,
+                result: tinyUrl,
+                quality: resultFix[0].quality,
+                size: resultFix[0].size,
+                thumb,
+                views,
+                channel,
+                uploadDate,
+                desc
+            });
+        }).catch(err => {
+          resolve()
+      })
+    }));
+};
 
-module.exports = { lirikLagu, covid, wikiSearch }
+function ytMp3(url) {
+    return new Promise((resolve, reject) => {
+        ytdl.getInfo(url).then(async(getUrl) => {
+            let result = [];
+            for(let i = 0; i < getUrl.formats.length; i++) {
+                let item = getUrl.formats[i];
+                if (item.mimeType == 'audio/webm; codecs=\"opus\"') {
+                    let { contentLength } = item;
+                    let bytes = await bytesToSize(contentLength);
+                    result[i] = {
+                        audio: item.url,
+                        size: bytes
+                    };
+                };
+            };
+            let resultFix = result.filter(x => x.audio != undefined && x.size != undefined) 
+            let tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].audio}`);
+            let tinyUrl = tiny.data;
+            let title = getUrl.videoDetails.title;
+            let desc = getUrl.videoDetails.description;
+            let views = millify(getUrl.videoDetails.viewCount)
+            let channel = getUrl.videoDetails.ownerChannelName;
+            let uploadDate = getUrl.videoDetails.uploadDate;
+            let thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
+            resolve({
+                title,
+                result: tinyUrl,
+                size: resultFix[0].size,
+                thumb,
+                views,
+                channel,
+                uploadDate,
+                desc
+            });
+          }).catch(err => {
+            resolve()
+        })
+    });
+          }
+
+module.exports = { lirikLagu, covid, wikiSearch, ytMp3, ytMp4 }
