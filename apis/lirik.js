7a44e787f564db194ab29c7deb7acbe18a9c4340
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2023-08-21 19:37:06 -0300

    Update lirik.js

--- apis/lirik.js
+++ apis/lirik.js
@@ -132,4 +132,18 @@ const ytMp3 = async (url) => {
         })
           }
 
-module.exports = { lirikLagu, covid, wikiSearch, ytMp3, ytMp4 }
+const mediafireDl = async (url) => {
+const res = await axios.get(url) 
+const $ = cheerio.load(res.data)
+const hasil = []
+const link = $('a#downloadButton').attr('href')
+const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '')
+const seplit = link.split('/')
+const nama = seplit[5]
+mime = nama.split('.')
+mime = mime[1]
+hasil.push({ nama, mime, size, link })
+return hasil
+}
+
+module.exports = { lirikLagu, covid, wikiSearch, ytMp3, ytMp4, mediafireDl }
